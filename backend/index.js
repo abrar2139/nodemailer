685839210575ce0c1592ce861fa6c3cea3cfee27
abrar2139 <mabrarqadri3@mail.com>
2024-03-20 13:20:21 +0500

    port changes

--- backend/index.js
+++ backend/index.js
@@ -7,7 +7,7 @@ const cors = require("cors")
 dotenv.config();
 const app = express();
 var corsOptions = {
-  origin: "http://localhost:3001"
+  origin: "http://localhost:3000"
 };
 
 app.use(cors(corsOptions));
