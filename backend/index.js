156f0327b035cd458e43fdb2bcfd519a5f7f0756
abrar2139 <mabrarqadri3@mail.com>
2024-03-20 16:38:29 +0500

    cors new changes

--- backend/index.js
+++ backend/index.js
@@ -16,10 +16,10 @@ app.use(cors());
 app.use(express.json());
 app.use("/", router);
 
-app.use((req, res, next) => {
-  res.header('Access-Control-Allow-Origin', '*');
-  next();
-});
+// app.use((req, res, next) => {
+//   res.header('Access-Control-Allow-Origin', '*');
+//   next();
+// });
 
 
 app.listen(Port, 
