3543d361e71ce42cbe5a24b33968376afc5f45ac
abrar2139 <mabrarqadri3@mail.com>
2024-03-22 21:22:09 +0500

    vercel allow origin add

--- backend/index.js
+++ backend/index.js
@@ -11,7 +11,7 @@ const app = express();
 //   corsOptions
 // };
 
-app.use(cors({origin:"https://nodemailer-frontend.vercel.app"}));
+app.use(cors());
 
 // app.get('/cors', (req, res) =>{
 //   res.set('Access-Control-Allow-Origin','*')
